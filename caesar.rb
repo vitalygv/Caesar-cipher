d6e2a773ccc364cf6a6caf9b727d93579495f307
vitalygv <kewl.dkmr@gmail.com>
2021-03-18 17:52:09 +0300

    indentation fixed

--- caesar.rb
+++ caesar.rb
@@ -1,16 +1,17 @@
 def caesar_cipher(str, offset)
-    result = str.bytes.map do |char|
-        if char.between?(65,90) 
-            char = (char + offset - 65) % 26 + 65
-            char.chr
-        elsif char.between?(97,122)
-            char = (char + offset - 97) % 26 + 97
-            char.chr
-        else
-            char.chr
-        end
+  result = str.bytes.map do |char|
+    if char.between?(65,90) 
+      char = (char + offset - 65) % 26 + 65
+      char.chr
+    elsif char.between?(97,122)
+      char = (char + offset - 97) % 26 + 97
+      char.chr
+    else
+      char.chr
     end
-   p result = result.join('')
+  end
+  
+  p result = result.join('')
 end
 
 caesar_cipher("What a string!", 5)
